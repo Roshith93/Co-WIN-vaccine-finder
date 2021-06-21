08249468df61ce0f47882db8aade6eb42cbdaab4
Roshith R <r.roshith@gmail.com>
2021-06-21 12:36:27 +0530

    Added state to dosage

--- src/Context/CowinContext.js
+++ src/Context/CowinContext.js
@@ -3,6 +3,7 @@ import {  createContext, useState, useEffect } from 'react'
 export const CowinContext = createContext()
 
 export const CowinProvider = ({ children }) => {
+  const [dosage, setDosage] = useState('Dosage1')
   return (
     <CowinContext.Provider value={'hello'}>{children}</CowinContext.Provider>
   )
