12b8fe8ec2ef1ff04f3e65c4642ed92e74c69aad
Roshith R <r.roshith@gmail.com>
2021-06-25 09:23:49 +0530

    booking slots design completed

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,12 +3,14 @@ import { ThemeProvider } from '@material-ui/core/styles'
 
 import { typographyTheme } from '../utils/styles'
 
-export const Heading = ({ variant, title, isCenter }) => (
+export const Heading = ({ variant, title, isCenter, component, color }) => (
   <ThemeProvider theme={typographyTheme}>
     <Typography
       variant={variant}
       gutterBottom
       style={isCenter ? { textAlign: 'center' } : null}
+      component={component}
+      color={color}
     >
       {title}
     </Typography>
--- src/container/VaccineSlots.js
+++ src/container/VaccineSlots.js
@@ -1,14 +1,19 @@
+import { useContext } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+import { v4 as uuidv4 } from 'uuid'
 
+import { Heading } from '../components/Heading'
+import { CowinContext } from '../context/CowinContext'
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    marginBottom: 12,
   },
   bullet: {
     display: 'inline-block',
@@ -24,35 +29,60 @@ const useStyles = makeStyles({
 })
 
 export const VaccineSlots = () => {
+  const { sessions } = useContext(CowinContext)
   const classes = useStyles()
   const bull = <span className={classes.bullet}>•</span>
 
   return (
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color='textSecondary'
-            gutterBottom
-          >
-            Word of the Day
-          </Typography>
-          <Typography variant='h5' component='h2'>
-            be{bull}nev{bull}o{bull}lent
-          </Typography>
-          <Typography className={classes.pos} color='textSecondary'>
-            adjective
-          </Typography>
-          <Typography variant='body2' component='p'>
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size='small'>Learn More</Button>
-        </CardActions>
-      </Card>
-    )
-  
+    sessions &&
+    sessions.map((session) => {
+      let {
+        center_id,
+        name,
+        address,
+        min_age_limit,
+        vaccine,
+        fee_type,
+        fee,
+        slots,
+        available_capacity,
+        available_capacity_dose1,
+        available_capacity_dose2,
+      } = session
+      console.log(typeof min_age_limit)
+      if (min_age_limit === 18) {
+        min_age_limit = '18-44'
+        console.log(min_age_limit)
+      }
+      if (min_age_limit === 45) {
+        min_age_limit = '45+'
+        console.log(min_age_limit)
+      }
+      return (
+        <Card className={classes.root} key={uuidv4()}>
+          <CardContent>
+            <Heading variant='h5' component='h2' title={`Center: ${name}`} />
+            <Heading title={`Address: ${address}`} color='' gutterBottom />
+            <Heading title={`Age: ${min_age_limit}`} color='' gutterBottom />
+            <Heading
+              title={`Vaccine: ${vaccine} ${' '} ${fee_type}`}
+              color=''
+              gutterBottom
+            />
+          </CardContent>
+          <CardActions>
+            {available_capacity.length ? (
+              <Button variant='contained' disabled>
+                No Slots Available
+              </Button>
+            ) : (
+              <Button variant='contained' color='primary'>
+                Book Now
+              </Button>
+            )}
+          </CardActions>
+        </Card>
+      )
+    })
+  )
 }
