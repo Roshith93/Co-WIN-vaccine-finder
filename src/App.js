6d93dc2057573092a8571ec05e74202f7a694b19
Roshith R <r.roshith@gmail.com>
2021-06-25 07:48:45 +0530

    removed react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { NavBar } from './components/NavBar'
 import { VaccineFinder } from './container/VaccineFinder'
-import { VaccineSlots } from './container/VaccineSlots'
 
 function App() {
   return (
     <>
       <NavBar title='Co-WIN Vaccine Finder' />
-      <VaccineFinder/>
-      
+      <VaccineFinder />
     </>
   )
 }
