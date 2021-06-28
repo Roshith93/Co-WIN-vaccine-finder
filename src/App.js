e445f734412f2001534847f418c92ac376df7c1c
Roshith R <r.roshith@gmail.com>
2021-06-28 07:44:18 +0530

    added warning text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { NavBar } from './components/NavBar'
 import { VaccineFinder } from './container/VaccineFinder'
+import WarningText  from './components/WarningText'
+
 
 function App() {
   return (
     <>
       <NavBar title='CO-WIN Vaccine Finder' />
       <VaccineFinder />
+      <WarningText />
+
     </>
   )
 }
---
+++ src/components/WarningText.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: 'grid',
+    placeItems: 'center',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    display: 'grid',
+    placeItems: 'center',
+    fontSize:10,
+    backgroundColor:'#74bbfb'
+  },
+}))
+
+export default function WarningText() {
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth='md' className={classes.homeContainer}>
+      <div className={classes.root}>
+        <Grid
+          container
+          spacing={3}
+          direction='row'
+          justify='center'
+          alignItems='center'
+          maxWidth='md'
+        >
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              This 'Cowin Vaccine Finder' app is only facilitating the search
+              experience through CoWIN public APIs. You will be re-directed to
+              CoWin to enter your log-in details in order to complete the
+              bookings. Availability of vaccination slots is subject to real
+              time data provided by state Government and private hospitals via
+              CoWIN. All information entered on the CoWin portal is handled as
+              per the Government data handling guidelines, and 'Cowin Vaccine
+              Finder' is not privy to this information.
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  )
+}
